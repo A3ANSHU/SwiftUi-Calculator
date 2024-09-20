4944019ca6df9d874e92f6723edffe19bdb3b5d1
amanbq <aman@bloqcube.com>
2024-09-20 16:35:48 +0530

    Initial Commit

---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by ABHINANDAN AMBEKAR on 20/09/24.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
